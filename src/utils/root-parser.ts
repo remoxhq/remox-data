960137f17700dfa82b0a68206a893bfe99c35f97
ilhambagirov <ilhambb@code.edu.az>
2024-02-08 03:48:59 +0400

    checks

--- src/utils/root-parser.ts
+++ src/utils/root-parser.ts
@@ -1,4 +1,4 @@
-import { CovalentClient, Chain } from "../libs/covalent"
+import { CovalentClient, Chain, PortfolioResponse } from "../libs/covalent"
 import { Organization } from "../libs/firebase-db"
 import { config } from "dotenv"
 import { TreasuryIndexer } from "../models/treasuries/types";
@@ -17,20 +17,20 @@ export const rootParser = async (dao: Organization, historicalTreasury: Treasury
             console.log("############walletAnnualPortfolioBalance#############");
             console.log(wallet);
 
-            const walletAnnualPortfolioBalance = await covalentClient.BalanceService.getHistoricalPortfolioForWalletAddress(
-                wallet.network as Chain,
-                wallet.address,
-                {
-                    quoteCurrency: 'USD',
-                    days: 365,
-                }
-            )
+            const response = await fetch(
+                `https://api.covalenthq.com/v1/${wallet.network as Chain}/address/${wallet.address}/portfolio_v2/?key=${covalentApiKey}&quote-currency=usd&days=${365}`,
+                { method: 'GET' }
+            );
+            const walletAnnualPortfolioBalance = await response.json() as { data: PortfolioResponse }
+            console.log(walletAnnualPortfolioBalance);
+
             walletAnnualPortfolioBalance.data.items?.map((token) => {
                 token.holdings.forEach((holding, index) => {
                     if (!holding.close.pretty_quote || index === 0) return;  // skip if there's no pretty_quote value
 
                     //split date and parse amount
-                    const date = holding.timestamp.toISOString().split("T")[0];
+                    const date = holding.timestamp.toString().split("T")[0];
+                    console.log(date);
                     const originAmount = holding.close?.quote ?? 0;
                     const amount = originAmount < 0 ? 0 : originAmount
                     const { contract_ticker_symbol } = token;
